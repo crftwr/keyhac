8dd4316e3de85040714976ebeb21487b8d96a542
craftware <craftware@gmail.com>
2026-08-30 00:07:54 -0700

    Releasing 2.3.0

--- keyhac/__init__.py
+++ keyhac/__init__.py
@@ -3,7 +3,7 @@
 Configuration files do `from keyhac import *` and get the user-facing API.
 """
 
-__version__ = "2.2.3"
+__version__ = "2.3.0"
 
 from keyhac.core.keymap import Keymap
 from keyhac.core.key import KeyCondition, KeyTable
